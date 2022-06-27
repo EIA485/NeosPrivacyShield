fcd0eb803271e34f1ef048c467e492a5af61ace3
eia485 <eia485git@gmail.com>
2022-06-27 11:49:43 -0700

    Add project files.

---
+++ PrivacyShield/PrivacyShield.cs
@@ -0,0 +1,49 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using FrooxEngine;
+using System;
+using System.Threading.Tasks;
+
+namespace PrivacyShield
+{
+    public class PrivacyShield : NeosMod
+    {
+        public override string Name => "PrivacyShield";
+        public override string Author => "ljoonal, eia485";
+        public override string Version => "0.3.1";
+        public override string Link => "https://github.com/EIA485/NeosPrivacyShield/";
+
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("xyz.ljoonal.neos.privacyshield");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(AssetManager), nameof(AssetManager.RequestGather))]
+        private class PrivacyShieldPatch
+        {
+
+            private static bool Prefix(AssetManager __instance, AssetGatherer ___assetGatherer, ref ValueTask<string> __result, Uri __0, FrooxEngine.Priority __1, CloudX.Shared.NeosDB_Endpoint? __2)
+            {
+                __result = HandleRequest(__instance, ___assetGatherer, __0, __1, __2);
+                return false;
+            }
+        }
+
+        private static async ValueTask<string> HandleRequest(AssetManager assetManager, AssetGatherer assetGatherer, Uri uri, FrooxEngine.Priority priority, CloudX.Shared.NeosDB_Endpoint? endpointOverwrite)
+        {
+            if (uri.Scheme == "neosdb" || uri.Scheme == "local" || await AskForPermission(assetManager.Engine, uri, "PrivacyShield generic request"))
+                return await assetGatherer.Gather(uri, priority, endpointOverwrite);
+            else
+                throw new Exception("No permissions to load asset");
+        }
+
+        private static async Task<bool> AskForPermission(Engine engine, Uri target, String accessReason)
+        {
+            Debug("Asking permissions for", target);
+            HostAccessPermission perms = await
+            engine.Security.RequestAccessPermission(target.Host, target.Port, accessReason);
+            return perms == HostAccessPermission.Allowed;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# PrivacyShield
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that forces all requests to create a host access popup.
+
+this is a fork of a mod originaly created by [ljoonal](https://github.com/ljoonal) the original source can be found [here](https://git.ljoonal.xyz/ljoonal/Neos-Mods/src/branch/main/PrivacyShield)
+
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+1. Place [PrivacyShield.dll](https://github.com/eia485/NeosPrivacyShield/releases/latest/download/PrivacyShield.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Neos logs.
